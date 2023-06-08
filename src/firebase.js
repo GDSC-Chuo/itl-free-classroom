3b7ce92fb9968639ccd747c22fe86b4254b17c52
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-06-08 13:45:15 +0900

    fix api key

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCoXYA1QbZwnCckbRqqZ0popV9eyCiUyhg",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "gdsc-team3.firebaseapp.com",
   projectId: "gdsc-team3",
   storageBucket: "gdsc-team3.appspot.com",
