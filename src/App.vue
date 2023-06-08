c38d8059d8ea1795f1f142832e6c0480ab679684
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-06-08 17:21:34 +0900

    fix css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div class="main-area">
+    <div class="contents">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -11,12 +13,31 @@ export default {
 </script>
 
 <style>
-#app {
+body {
+  background-color: #3A3E3B;
+}
+.main-area {
+  position: absolute;
+  top: calc(50% - 45vh);
+  left: calc(50% - 45vw);
+  width: 90vw;
+  height: 90vh;
+  background: #FFFFFF;
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 30px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #3A3E3B;
+}
+.contents {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 16px;
+}
+p {
+  margin: 0;
 }
 </style>
--- src/components/3rdFloor.vue
+++ src/components/3rdFloor.vue
@@ -1,6 +1,8 @@
 <template>
-  <div >
-    <h1>3kai</h1>
+  <div class="component-area">
+    <div class="title">
+      <p>3F</p>
+    </div>
     <img src="@/assets/3F.png" alt="3F" >
   </div>
 </template>
@@ -13,5 +15,27 @@ export default {
 </script>
 
 <style scoped>
-
+.component-area {
+}
+.title {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 40px;
+  padding-right: 40px;
+  border: 2px solid #747578;
+  border-radius: 80px;
+}
+.title > p {
+  color: #ff0000;
+  font-weight: 700;
+  font-size: 32px;
+  margin: 0s;
+}
+img {
+  max-width: calc(90vw - 32px);
+  max-height: calc(90vh - 32px);
+}
 </style>
