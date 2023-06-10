4bda65fcb13fcfae551e31b344277694c5ea4aac
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-06-10 23:41:47 +0900

    add current time

--- src/components/3rdFloor.vue
+++ src/components/3rdFloor.vue
@@ -3,15 +3,59 @@
     <div class="title">
       <p>3F</p>
     </div>
+    <div v-if="currentClass">
+      <h2>現在の授業</h2>
+      <p>{{ currentClass.name }}</p>
+    </div>
+    <div v-else>
+      <p>該当授業なし</p>
+    </div>
     <img src="@/assets/3F.png" alt="3F" >
     <router-link to="/floor-select" class="back">フロア選択へ戻る</router-link>
   </div>
 </template>
 
 <script>
+import { db } from '../firebase'
+import { onSnapshot, collection } from 'firebase/firestore';
 
 export default {
-  name: '3rdFloor'
+  name: '3rdFloor',
+  data() {
+    return {
+      classes: [],
+      periods: [
+        { start: '09:00', end: '10:40' },
+        { start: '10:50', end: '12:30' },
+        { start: '13:20', end: '15:00' },
+        { start: '15:10', end: '16:50' },
+        { start: '23:00', end: '23:50' },
+        { start: '18:50', end: '20:30' },
+      ],
+    }
+  },
+  created() {
+    const classesRef = collection(db, 'class information');
+    onSnapshot(classesRef, (snapshot) => {
+      this.classes = snapshot.docs.map((doc) => doc.data());
+      console.log(this.classes);
+    });
+  },
+  computed: {
+    currentClass() {
+      const now = new Date();
+      const currentPeriod = this.periods.find((period) => {
+        const startTime = new Date(`${now.toISOString().split('T')[0]}T${period.start}`);
+        const endTime = new Date(`${now.toISOString().split('T')[0]}T${period.end}`);
+        return now >= startTime && now <= endTime;
+      });
+
+      if (!currentPeriod) return null;
+
+      const periodIndex = this.periods.indexOf(currentPeriod);
+      return this.classes.find((c) => c.period === periodIndex + 1);
+    },
+  }
 }
 </script>
 
