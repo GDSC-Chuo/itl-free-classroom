b9349f09e383bdedd47aee0fc1fd13938b1d5e48
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2023-06-11 22:43:26 +0900

    add day info

--- src/components/3rdFloor.vue
+++ src/components/3rdFloor.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <div class="title">
-      <p>3F</p>
-    </div>
-    <div v-if="currentClass">
-      <h2>現在の授業</h2>
-      <p>{{ currentClass.name }}</p>
-    </div>
-    <div v-else>
-      <p>該当授業なし</p>
+    <div class="floor">
+      <div class="title">
+        <p>3F</p>
+      </div>
+      <div class="floor-info">
+        <div v-if="currentClass">
+          <h2>現在の授業</h2>
+          <p>{{ currentClass.subject }}</p>
+        </div>
+        <div v-else>
+          <p>現在３Fで実施されている授業はありません。</p>
+        </div>
+      </div>
     </div>
     <img src="@/assets/3F.png" alt="3F" >
     <router-link to="/floor-select" class="back">フロア選択へ戻る</router-link>
@@ -29,7 +33,7 @@ export default {
         { start: '10:50', end: '12:30' },
         { start: '13:20', end: '15:00' },
         { start: '15:10', end: '16:50' },
-        { start: '23:00', end: '23:50' },
+        { start: '17:00', end: '18:40' },
         { start: '18:50', end: '20:30' },
       ],
     }
@@ -42,6 +46,16 @@ export default {
     });
   },
   computed: {
+    currentDay() {
+      // 現在の曜日を取得（0（日曜日）から6（土曜日）までの数値）
+      const now = new Date();
+      const dayIndex = now.getDay();
+
+      // 曜日の数値を日本語の曜日名に変換
+      const daysInJapanese = ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'];
+      const currentDay = daysInJapanese[dayIndex];
+      return currentDay;
+    },
     currentClass() {
       const now = new Date();
       const currentPeriod = this.periods.find((period) => {
@@ -53,23 +67,28 @@ export default {
       if (!currentPeriod) return null;
 
       const periodIndex = this.periods.indexOf(currentPeriod);
-      return this.classes.find((c) => c.period === periodIndex + 1);
+      console.log(this.classes.find((c) => c.period === periodIndex + 1));
+      return this.classes.find((c) => c.period === periodIndex + 1 && c.day === this.currentDay);
     },
   }
 }
 </script>
 
 <style scoped>
-.title {
+.floor {
   position: absolute;
   left: 20px;
   top: 20px;
+
+}
+.title {
   padding-top: 10px;
   padding-bottom: 10px;
   padding-left: 40px;
   padding-right: 40px;
   border: 2px solid #747578;
   border-radius: 80px;
+  width: 38px;
 }
 /* titleというdivの中にあるpの設定  */
 .title > p {
@@ -77,6 +96,9 @@ export default {
   font-weight: 700;
   font-size: 32px;
   margin: 0s;
+}
+.floor-info {
+  margin-top: 30px;
 }
 img {
   max-width: calc(90vw - 32px);
