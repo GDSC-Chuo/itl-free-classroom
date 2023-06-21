1171161893a1cd25071714f2fd8d2334a57ae8d6
yutti2525 <yuu.nagahama2525@gmail.com>
2023-06-21 16:14:01 +0900

    add search class

---
+++ src/components/SearchClass.vue
@@ -0,0 +1,25 @@
+<!-- TODO: デザイン修正、ログインする/ログインせずに使うというリンクを用意する -->
+<template>
+    <div >
+      <h1>Class Information</h1>
+    </div>
+    
+      <h3><input type="text" name="subject" placeholder="Input Information">
+      </h3>
+
+
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
+  <style scoped>
+  h3 {
+    text-align: center;
+  }
+  
+  </style>
