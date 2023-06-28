accfe8f30d695cfd6579678f741a15766c8eeec6
haipa-nishi <108124535+haipa-nishi@users.noreply.github.com>
2023-06-28 15:22:46 +0900

    Merge branch 'main' of https://github.com/RinKokubo/team3-app

--- src/components/ClassInfo.vue
+++ src/components/ClassInfo.vue
@@ -2,20 +2,37 @@
 <template>
   <div >
     <h1>授業情報</h1>
-    <h2>{{ classData.subject }}</h2>
-    <h2>教員： 飯尾淳</h2>
-    <h3>学期： 前期</h3>
+
+    <h2>科目名：{{ classData.subject }}</h2>
+    <h3 v-if="classData.required">【必修科目】</h3>
+    <h3 v-else>【選択科目】</h3>
+    <h2>担当教員：{{ classData.teacher }}</h2>
+    <h3>学期：{{ classData.semester }}</h3>
+    
+
     <div>
-      <h3>履修可能学年</h3>
-      <p>1年: 必修</p>
-      <p>2年: 可</p>
-      <p>3年: 不可</p>
-      <p>4年</p>
+      <h3>配当年次</h3>
+      <p v-if="classData['1st year'] && classData.required">1年次：必修</p>
+      <p v-else-if="classData['1st year']">1年次：可</p>
+      <p v-else>1年次：不可</p>
+
+      <p v-if="classData['2nd year'] && classData.required">2年次：必修</p>
+      <p v-else-if="classData['2nd year']">2年次：可</p>
+      <p v-else>2年次：不可</p>
+
+      <p v-if="classData['3rd year'] && classData.required">3年次：必修</p>
+      <p v-else-if="classData['3rd year']">3年次：可</p>
+      <p v-else>3年次：不可</p>
+
+      <p v-if="classData['4th year'] && classData.required">4年次：必修</p>
+      <p v-else-if="classData['4th year']">4年次:可</p>
+      <p v-else>4年次：不可</p>
     </div>
     <div>
       <h3>授業実施日時</h3>
-      <p>水曜 ４限</p>
+      <p>{{ classData.day }} {{ classData.period }}時限</p>
     </div>
+    <router-link to="/floor-select" class="back">フロア選択へ戻る</router-link>
   </div>
 </template>
 
@@ -28,5 +45,27 @@ export default {
 </script>
 
 <style scoped>
+.button-container {
+  position: fixed;
+  bottom: 200px;
+  right: 200px;
+}
+
+.back {
+  position: absolute;
+  right: 25px;
+  bottom: 25px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 40px;
+  padding-right: 40px;
+  border: 2px solid #747578;
+  border-radius: 80px;
+  color: #ff0000;
+  font-weight: 700;
+  font-size: 16px;
+  background-color: #fff;
+  text-decoration: none;
+}
 
 </style>
