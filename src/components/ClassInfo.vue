908e0ff30a0ccc65a7115ea15810ea03543fec54
haipa-nishi <108124535+haipa-nishi@users.noreply.github.com>
2023-07-05 16:32:30 +0900

    css-fixed

--- src/components/ClassInfo.vue
+++ src/components/ClassInfo.vue
@@ -1,81 +1,97 @@
 <!-- TODO: デザイン修正、フロアガイドに戻るボタンとフロア選択に戻るボタン追加-->
 <template>
-  <div >
+  <div class="all">
     <h1 class="class-info1">授業情報</h1>
 
     <h2 class="class-info">科目名：{{ classData.subject }}</h2>
     <h2 class="class-info">担当教員：{{ classData.teacher }}</h2>
-    <h3 class="class-info">学期：{{ classData.semester }}</h3>
-    <h3 class="class-info">曜日時限：{{ classData.day }}{{ classData.period }}時限</h3>
-      
+    <h3 class="class-info">学期 ：{{ classData.semester }}</h3>
+    <h3 class="class-info">
+      曜日時限：{{ classData.day }}{{ classData.period }}時限
+    </h3>
 
     <div>
       <h3 class="year">配当年次</h3>
-      <p v-if="classData['1st year'] && classData.required" class="year">1年次：必修</p>
-      <p v-else-if="classData['1st year']" class="year">1年次：可</p>
-      <p v-else class="year">1年次：不可</p>
+      <p v-if="classData['1st year'] && classData.required" class="year">
+        1年次：必修
+      </p>
+      <p v-else-if="classData['1st year']" class="year">1年次</p>
+      <p v-else class="year"></p>
 
-      <p v-if="classData['2nd year'] && classData.required" class="year">2年次：必修</p>
-      <p v-else-if="classData['2nd year']" class="year">2年次：可</p>
-      <p v-else class="year">2年次：不可</p>
+      <p v-if="classData['2nd year'] && classData.required" class="year">
+        2年次：必修
+      </p>
+      <p v-else-if="classData['2nd year']" class="year">2年次</p>
+      <p v-else class="year"></p>
 
-      <p v-if="classData['3rd year'] && classData.required" class="year">3年次：必修</p>
-      <p v-else-if="classData['3rd year']" class="year">3年次：可</p>
-      <p v-else class="year">3年次：不可</p>
+      <p v-if="classData['3rd year'] && classData.required" class="year">
+        3年次：必修
+      </p>
+      <p v-else-if="classData['3rd year']" class="year">3年次</p>
+      <p v-else class="year"></p>
 
-      <p v-if="classData['4th year'] && classData.required" class="year">4年次：必修</p>
-      <p v-else-if="classData['4th year']" class="year">4年次:可</p>
-      <p v-else class="year">4年次：不可</p>
+      <p v-if="classData['4th year'] && classData.required" class="year">
+        4年次：必修
+      </p>
+      <p v-else-if="classData['4th year']" class="year">4年次</p>
+      <p v-else class="year"></p>
     </div>
-    
+
     <router-link to="/floor-select" class="back">戻る</router-link>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'ClassInfo',
-  props: ['id', 'classData']
-}
+  name: "ClassInfo",
+  props: ["id", "classData"],
+};
 </script>
 
 <style scoped>
-.class-info{
-display: flex;
-width: 515px;
-flex-direction: column;
-justify-content: center;
-flex-shrink: 0;
-color: #000;
-text-align: center;
-font-size: 36px;
-font-family: Zen Kaku Gothic Antique;
-margin: 0;
+.all {
+  display: flex;
+  flex-direction: column;
+  padding: 150px;
 }
 
-.class-info1{
-display: flex;
-width: 526px;
-height: 149px;
-flex-direction: column;
-justify-content: center;
-flex-shrink: 0;
-color: #000;
-text-align: center;
-font-size: 50px;
-font-family: Zen Maru Gothic;
-font-weight: 700;
+.class-info1 {
+  display: flex;
+  width: 526px;
+  height: 149px;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: #000;
+  font-size: 50px;
+  font-family: Zen Maru Gothic;
+  font-weight: 700;
+  margin: 0;
+  text-align: center;
 }
 
-.year{
-color: #000;
-text-align: center;
-font-size: 20px;
-font-family: Zen Kaku Gothic Antique;
-text-align: left;
+.class-info {
+  display: flex;
+  width: 515px;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: #000;
+  font-size: 36px;
+  font-family: Zen Kaku Gothic Antique;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin: 0;
+  text-align: center;
 }
 
+.year {
+  color: #000;
+  font-size: 20px;
+  font-family: Zen Kaku Gothic Antique;
+  text-align: left;
+}
 .button-container {
   position: fixed;
   bottom: 200px;
@@ -98,5 +114,4 @@ text-align: left;
   background-color: #fff;
   text-decoration: none;
 }
-
 </style>
