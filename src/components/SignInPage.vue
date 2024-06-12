a3e1d7ccfe1f9c028848e3f7634d3efd4af9e1cc
rin-personal <taka0102rin@gmail.com>
2024-06-13 00:38:52 +0900

    style: 拡大縮小してもスタイルを保てるよう修正

--- src/components/SignInPage.vue
+++ src/components/SignInPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <h1 class="signin">Sign in</h1>
+      <h1 class="signin-title">Sign in</h1>
     </div>
 
     <form @submit.prevent="loginUser">
@@ -13,14 +13,32 @@
       </div>
       <div><input type="submit" id="signin-button" class="login" value="ログイン"></div>
     </form>
-    <div class="return">
+    <div class="signin-return">
       <router-link to="/" class="return-box">戻る</router-link>
     </div>
   </div>
 </template>
 
 <style>
+.signin-title {
+  position: relative;
+  text-align: center;
+  padding-top: 20vh;
+  display: inline-block;
+  font-size: 40px;
+  line-height: 50px
+}
+
+.signin-id {
+  position: relative;
+}
+
+.signin-password {
+  position: relative;
+}
+
 .login {
+  position: relative;
   font-size: 22px;
   width: 200px;
   height: 50px;
@@ -29,11 +47,19 @@
   color: white;
   border-color: #FF0000;
   text-align: center;
-  margin-top: 120px;
+  display: inline-block;
+  margin-top: 10vh;
 }
 
 .login:hover {
   opacity: .7;
+}
+
+.signin-return {
+  position: relative;
+  text-align: center;
+  margin-top: 200px;
+  /* ログインボタン最下部からの距離 */
 }
 </style>
 
