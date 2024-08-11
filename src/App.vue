8fc9eadc5f621aaf4cd4ce97fe08cffe0b8d3686
rin-personal <taka0102rin@gmail.com>
2024-08-11 18:22:42 +0900

    style: change font

--- src/App.vue
+++ src/App.vue
@@ -24,46 +24,45 @@ export default {
 
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=BIZ+UDPGothic&family=Zen+Kaku+Gothic+New&display=swap');
+
 body {
   background-color: #3A3E3B;
+  font-family: "Zen Kaku Gothic New", sans-serif;
 }
 
 .main-area {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 1200px;
-    height: 1000px;
-    background: #FFFFFF;
-    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);
-    border-radius: 30px;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #3A3E3B;
-    overflow: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 1200px;
+  height: 1000px;
+  background: #FFFFFF;
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 30px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #3A3E3B;
+  overflow: auto;
 }
 
 /* 画面が小さくなったときの調整 */
 @media (max-width: 1300px) {
-    .main-area {
-        width: 90%;
-        /* 画面幅の90% */
-        height: 80%;
-        /* 画面高さの80% */
-    }
+  .main-area {
+    width: 90%;
+    height: 80%;
+  }
 }
 
 /* スマホ版のスタイル */
 @media (max-width: 600px) {
-    .main-area {
-        width: 95%;
-        top: 5%;
-        left: 50%;
-        transform: translateX(-50%);
-        margin-bottom: 5%;
-    }
+  .main-area {
+    width: 95%;
+    top: 5%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 }
 
 .special-area {
@@ -76,7 +75,6 @@ body {
   background: #FFFFFF;
   box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);
   border-radius: 30px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #3A3E3B;
