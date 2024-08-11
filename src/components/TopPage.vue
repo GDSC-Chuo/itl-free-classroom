9d95bac1c7b91aa5f9049b1e93445d45a02af53f
rin-personal <taka0102rin@gmail.com>
2024-08-11 18:22:07 +0900

    feat: 新規ページ

---
+++ src/components/TopPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="top-change">
+    <router-link to="/floor-select" class="choice-box">空き教室情報</router-link>
+    <router-link to="/search-class" class="choice-box">授業情報</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopPage",
+};
+</script>
+
+<style scoped>
+.top-change {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.choice-box {
+  position: relative;
+  margin: 40px 30px;
+  width: 400px;
+  height: 100px;
+
+  border: 2px solid #747578;
+  border-radius: 80px;
+  color: #ff0000;
+  font-weight: 500;
+  font-size: 48px;
+  vertical-align: middle;
+  background-color: #fff;
+  text-decoration: none;
+
+  text-align: center;
+  line-height: 100px;
+}
+
+.choice-box:hover {
+  background-color: #f5f5f5;
+}
+
+@media (max-width: 600px) {
+  .choice-box {
+    width: 200px;
+    height: 80px;
+    font-size: 24px;
+    line-height: 80px;
+  }
+}
+</style>
