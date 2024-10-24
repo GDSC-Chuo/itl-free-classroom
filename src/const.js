41f220e557561070dc47bcd0a383a00ce8ca8f73
manasan-iTL <daiyaofssh2020@gmail.com>
2024-10-24 09:50:18 +0900

    delete: ユーザー状態の反映が難しいためlocalstore系の処理を削除

--- src/const.js
+++
@@ -1,9 +0,0 @@
-export const authState = {
-    loggedIn: "LOGGEDIN",
-    loggedOut: "LOGGEDOUT",
-    unVerified: "UNVERIFIED"
-}
-
-export const LocalStorageKeys = {
-    auth: "ITL-FREE-CLASSROOM-AUTH"
-}
