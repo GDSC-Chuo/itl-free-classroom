606c38e26452a6f7854539d46734d23a35fef382
rin-personal <taka0102rin@gmail.com>
2024-10-26 11:48:32 +0900

    style, refactor: スタイル調整と戻るボタンの動作修正

--- src/components/ClassInfo.vue
+++ src/components/ClassInfo.vue
@@ -1,170 +1,165 @@
 <template>
-    <div class=“all”>
-        <h1 class="class-info1">授業情報</h1>
-        <div class=class-name>
-            <h2 class=class-info2>科目名</h2>
-            <h2 class=class-info>{{ classDataCopy.subject }}</h2>
-        </div>
-        <div class=class-name>
-            <h2 class=class-info2>担当教員</h2>
-            <h2 class=class-info>{{ classDataCopy.teacher }}</h2>
-        </div>
-        <div class=class-name>
-            <h2 class=class-info2>学期</h2>
-            <h2 class=class-info>{{ classDataCopy.semester }}</h2>
-        </div>
-        <div class=class-name>
-            <h2 class=class-info2>曜日時限</h2>
-            <h2 class=class-info>{{ classDataCopy.day }}{{ classDataCopy.period }}時限</h2>
-        </div>
-        
-
-        <router-link to=“/floor-select” class=back @click="goBack">戻る</router-link>
+  <div class="all">
+    <h1 class="classinfo-main">授業情報</h1>
+    <div class=class-name>
+      <h2 class=classinfo-title>科目名</h2>
+      <h2 class=class-info>{{ classDataCopy.subject }}</h2>
     </div>
+    <div class=class-name>
+      <h2 class=classinfo-title>担当教員</h2>
+      <h2 class=class-info>{{ classDataCopy.teacher }}</h2>
+    </div>
+    <div class=class-name>
+      <h2 class=classinfo-title>学期</h2>
+      <h2 class=class-info>{{ classDataCopy.semester }}</h2>
+    </div>
+    <div class=class-name>
+      <h2 class=classinfo-title>曜日時限</h2>
+      <h2 class=class-info>{{ classDataCopy.day }}{{ classDataCopy.period }}時限</h2>
+    </div>
+
+    <div class="back-floor"><button class="back-floor-box" @click="goBack">戻る</button></div>
+  </div>
 </template>
+
 <script>
-import { db } from '../firebase'
-import { doc, updateDoc, increment } from "firebase/firestore";
 export default {
-    name: "ClassInfo",
-    props: ["id", "classData"],
-    data() {
-        return {
-            isHeartRed: false,
-            classDataCopy: JSON.parse(JSON.stringify(this.classData)), // プロパティのコピーを作成
-        }
-    },
-    methods: {
-        async toggleHeartColor() {
-            const classRef = doc(db, 'class information', this.classDataCopy.id);
-            if (this.isHeartRed) {
-                this.isHeartRed = false;
-                // ドキュメントを更新して、いいねのカウントを1減らす
-                const decrement = increment(-1);
-                await updateDoc(classRef, { recommendedCount: decrement });
-                // 画面上の表示を更新する
-                this.classDataCopy.recommendedCount--;
-            } else {
-                this.isHeartRed = true;
-                // ドキュメントを更新して、いいねのカウントを1増やす
-                const incrementValue = increment(1);
-                await updateDoc(classRef, { recommendedCount: incrementValue });
-                // 画面上の表示を更新する
-                this.classDataCopy.recommendedCount++;
-            }
-        },
-        goBack() {
-            this.$router.go(-1);
-        }
+  name: "ClassInfo",
+  props: ["id", "classData"],
+  data() {
+    return {
+      isHeartRed: false,
+      classDataCopy: JSON.parse(JSON.stringify(this.classData)), // プロパティのコピーを作成
     }
+  },
+  computed: {
+    // クエリパラメータからフロア情報を取得
+    floor() {
+      return this.$route.query.floor || 'default';
+    }
+  },
+  methods: {
+    goBack() {
+      if (window.history.length > 1) {
+        this.$router.back(); // 履歴がある場合は戻る
+      } else {
+        // 履歴がない場合は、フロア情報を持ってフロア選択ページに遷移
+        this.$router.push({ name: 'FloorSelect', query: { floor: this.floor } });
+      }
+    }
+  }
 };
 </script>
+
 <style scoped>
-.all {
-    display: flex;
-    flex-direction: column;
+@import url('https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap');
+
+* {
+  font-family: "Zen Kaku Gothic New", sans-serif;
 }
 
-.class-info1 {
-    display: flex;
-    height: 149px;
-    flex-direction: column;
-    justify-content: center;
-    flex-shrink: 0;
-    color: #000;
-    font-size: 50px;
-    font-weight: 700;
-    margin: 0;
-    text-align: center;
+.classinfo-main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: #000;
+  font-size: 50px;
+  font-weight: 700;
+  text-align: center;
 }
 
 .class-name {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 40px;
-    flex-direction: column;
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  flex-direction: column;
 }
 
 .class-data-title {
-    font-weight: bold;
-}
-
-.recommend {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-
-.heart-icon {
-    border: 2px solid #FF0000;
-    border-radius: 22px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 40px;
-    height: 40px;
+  font-weight: bold;
 }
 
 .class-info {
-    display: flex;
-    width: 515px;
-    flex-direction: column;
-    justify-content: center;
-    flex-shrink: 0;
-    color: #000;
-    font-size: 36px;
-    font-style: normal;
-    font-weight: normal;
-    line-height: normal;
-    margin: 0;
-    text-align: center;
-    padding: 5px;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  color: #000;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: normal;
+  line-height: normal;
+  margin: 0;
+  text-align: center;
+  padding: 5px;
 }
 
-.class-info2 {
-    display: flex;
-    width: 515px;
-    flex-direction: column;
-    justify-content: center;
-    flex-shrink: 0;
-    color: #000;
-    font-size: 36px;
-    font-style: normal;
-    font-weight: 800;
-    line-height: normal;
-    margin: 0;
-    text-align: center;
-    padding: 5px;
+.classinfo-title {
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  color: #000;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  margin: 0;
+  text-align: center;
+  padding: 5px;
 }
 
 .year {
-    color: #000;
-    font-size: 20px;
-    text-align: left;
+  color: #000;
+  font-size: 20px;
+  text-align: left;
 }
 
 .button-container {
-    position: fixed;
-    bottom: 200px;
-    right: 200px;
+  position: fixed;
+  bottom: 200px;
+  right: 200px;
 }
 
-.back {
-    position: absolute;
-    right: 25px;
-    bottom: 25px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    padding-left: 40px;
-    padding-right: 40px;
-    border: 2px solid #747578;
-    border-radius: 80px;
-    color: #FF0000;
-    font-weight: 700;
+.back-floor {
+  position: relative;
+  text-align: center;
+  margin-top: 50px;
+}
+
+.back-floor-box {
+  position: relative;
+  padding: 10px 50px;
+  border: 2px solid #747578;
+  border-radius: 30px;
+  color: #ff0000;
+  font-size: 22px;
+  vertical-align: middle;
+  background-color: white;
+  text-decoration: none;
+  text-align: center;
+  display: inline-block;
+}
+
+.back-floor-box:hover {
+  background-color: #f5f5f5;
+}
+
+@media (max-width: 600px) {
+  .classinfo-main {
+    font-size: 32px;
+  }
+
+  .class-info {
+    font-size: 24px;
+  }
+
+  .classinfo-title {
+    font-size: 24px;
+  }
+
+  .back-floor-box {
     font-size: 16px;
-    background-color: #fff;
-    text-decoration: none;
+    padding: 10px 30px;
+  }
 }
 </style>
