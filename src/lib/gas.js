32dedb4607504e30ea65f3818b6cb7a7fdb70007
{hazuki} <{hazuki.onozuka8@gmail.com}>
2025-01-03 16:00:52 +0900

    GAS更新

--- src/lib/gas.js
+++ src/lib/gas.js
@@ -1,7 +1,7 @@
 // GASのWebアプリURL
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxY0Zr3nad7TKnKlN8S7u1BtwX8a63QzvATciGhVhy6J9owJswGaBAmC235-yHXrGpu/exec";
 // データを取得する関数
-export async function fetchDataFromGAS() {
+async function fetchDataFromGAS() {
   try {
     // GASからデータを取得
     const response = await fetch(GAS_API_URL);
@@ -18,7 +18,7 @@ export async function fetchDataFromGAS() {
   }
 }
 
-
+module.exports = { fetchDataFromGAS };
 
 // 関数を実行
 fetchDataFromGAS();
