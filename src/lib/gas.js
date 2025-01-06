c7296d74176c8184bbbd03b0ad66e1b538655b4c
{hazuki} <{hazuki.onozuka8@gmail.com}>
2025-01-06 13:15:37 +0900

    ミス修正

--- src/lib/gas.js
+++ src/lib/gas.js
@@ -2,7 +2,7 @@
 const GAS_API_URL ="https://script.google.com/macros/s/AKfycbxY0Zr3nad7TKnKlN8S7u1BtwX8a63QzvATciGhVhy6J9owJswGaBAmC235-yHXrGpu/exec";
 
 // データを取得する関数
-async function fetchDataFromGAS() {
+export async function fetchDataFromGAS() {
   try {
     // GASからデータを取得
     const response = await fetch(GAS_API_URL);
