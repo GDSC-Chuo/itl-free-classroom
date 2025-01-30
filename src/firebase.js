28f317e42f023a0392fc3ab8886d35fda37a9a6c
rin-personal <taka0102rin@gmail.com>
2025-01-31 00:08:37 +0900

    fix: firebase不具合修正

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from '@firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
